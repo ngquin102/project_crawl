6531e8268eead63eba69d7442f1002feae365bdc
ngquin102 <95980352+ngquin102@users.noreply.github.com>
2023-04-11 00:03:29 +0700

    Update craw.py

--- craw.py
+++ craw.py
@@ -19,10 +19,8 @@ html = driver.page_source
 # Phân tích cú pháp HTML
 soup = BeautifulSoup(html, "html.parser")
 audios = soup.find_all("div",class_="main--4+PDC")
-load_more_button = driver.find_element(By.CSS_SELECTOR,"a.discoverMoreButton--PZSrF")
 while True:
-    load_more_button.click()
-    time.sleep(5)
+    
     for audio in audios:
         name = audio.find("a",class_="name--q8l1g").text.strip()
         artist = audio.find("a", class_="artist--VkDWG").text.strip()
@@ -38,6 +36,10 @@ while True:
             #"Link":link
         })
         print(name,artist,time)
+    load_more_button = driver.find_element(By.CSS_SELECTOR,"a.discoverMoreButton--PZSrF")
+    if load_more_button:
+        load_more_button.click()
+        time.sleep(5)
     if not load_more_button:
         break
 # Tạo dataframe từ dữ liệu
